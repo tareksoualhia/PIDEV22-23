b355a306ffeba7f1d7869c1aa91cf160541904a3
tareksoualhia <80466724+tareksoualhia@users.noreply.github.com>
2023-02-19 19:14:49 +0100

    Update Formation.php

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use App\Repository\FormationRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: FormationRepository::class)]
 class Formation
 {
@@ -14,9 +14,22 @@ class Formation
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le nom est obligatoire.")
+     * @Assert\Length(max=255, maxMessage="Le nom ne doit pas dépasser {{ limit }} caractères.")
+     */
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="La description est obligatoire.")
+     * @Assert\Regex(
+     *     pattern="/^[a-z0-9\s]+$/",
+     *     message="La description ne doit contenir que des lettres minuscules, des chiffres et des espaces."
+     * )
+     */
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
@@ -25,19 +38,26 @@ class Formation
     #[ORM\Column(length: 255)]
     private ?string $meet = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $prix = null;
+    #[ORM\Column]
+     /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     * @Assert\NotBlank(message="Le prix est obligatoire.")
+     * @Assert\Regex(
+     *     pattern="/^[0-9]+$/",
+     *     message="Le prix doit être un entier positif."
+     * )
+     * @Assert\GreaterThanOrEqual(value=0, message="Le prix doit être positif.")
+     */
+    private ?int $prix = null;
 
-    #[ORM\ManyToOne(inversedBy: 'description')]
+    #[ORM\ManyToOne(inversedBy: 'formation')]
     private ?Category $category = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
-    public function __toString() {
-        return $this->id;
-    }
+
     public function getNom(): ?string
     {
         return $this->nom;
@@ -86,12 +106,12 @@ class Formation
         return $this;
     }
 
-    public function getPrix(): ?string
+    public function getPrix(): ?int
     {
         return $this->prix;
     }
 
-    public function setPrix(string $prix): self
+    public function setPrix(int $prix): self
     {
         $this->prix = $prix;
 
