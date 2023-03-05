471a1ff4ce3cf81cba1e57d58a0fbb1fb0f00cb9
tareksoualhia <80466724+tareksoualhia@users.noreply.github.com>
2023-03-05 13:58:35 +0100

    Add files via upload

---
+++ src/Entity/FormationLike.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationLikeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FormationLikeRepository::class)]
+class FormationLike
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'likes')]
+    private ?Formation $formation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFormation(): ?Formation
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(?Formation $formation): self
+    {
+        $this->formation = $formation;
+
+        return $this;
+    }
+}
