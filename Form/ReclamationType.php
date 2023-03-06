b559105035596ca47e080be68b870dd869e6cc4c
ebtyhellbenyehmed <92762676+ebtyhellbenyehmed@users.noreply.github.com>
2023-03-06 10:19:28 +0100

    Add files via upload

---
+++ Form/ReclamationType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Reclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+
+class ReclamationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+           //->add('Numero')
+           //->add('Nom')
+           //->add('Date')
+            ->add('Type',ChoiceType::class, [
+                'choices' => [
+                   
+                    'Reclamation Entraineur' => 'Reclamation Entraineur',
+                    'Reclamation Evenement' => 'Reclamation Evenement',
+                    'Reclamation competition' => 'Reclamation competition'
+
+                ],
+                'placeholder' => 'Selectionner une option ',
+            ])
+            ->add('Description',TextareaType::class)
+            ->add('Date');
+
+
+           /* ->add('Envoyer',SubmitType::class)
+            ->add('Annuler', ResetType::class, array(
+                'label' => 'Annuler',
+                'attr' => array('class' => 'btn btn-default'),
+            ));*/
+            
+            
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reclamation::class,'csrf_protection' => false,
+        ]);
+    }
+
+
+
+
+
+    
+}
---
+++ Form/ReponseType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reponse;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class ReponseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('reponse',TextareaType::class )
+            ->add('Etat',ChoiceType::class, [
+                'choices' => [
+                    'Ok' => 'Ok',
+                    'En Attente' => 'En Attente',
+                   
+
+                ],
+                'placeholder' => 'Selectionner une option ',
+             ])
+          // ->add('reclamation')
+          // ->add('envoyer',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reponse::class,
+        ]);
+    }
+}
